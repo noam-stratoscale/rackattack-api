b162b67fc3b510c239c54741f6e217209ffa046c
Shlomi Matichin <shlomi@stratoscale.com>
2014-07-10 15:03:31 +0300

    Command line 'playaround' tool to allocate one node

---
+++ py/rackattack/playaround.py
@@ -0,0 +1,35 @@
+import os
+from rackattack.ssh import connection
+from rackattack import clientfactory
+from rackattack import api
+connection.discardParamikoLogs()
+connection.discardSSHDebugMessages()
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--label", required=True)
+parser.add_argument("--user", required=True, help="User to report to provider")
+parser.add_argument("--nice", type=float, default=1)
+args = parser.parse_args()
+
+print "Connecting"
+client = clientfactory.factory()
+print "Allocating"
+allocation = client.allocate(
+    requirements={'node': api.Requirement(imageLabel=args.label, imageHint="playaround")},
+    allocationInfo=api.AllocationInfo(user=args.user, purpose="playaround", nice=args.nice))
+allocation.wait(timeout=5 * 60)
+assert allocation.done(), "Allocation failed"
+print "Done allocating"
+try:
+    node = allocation.nodes()['node']
+    credentials = node.rootSSHCredentials()
+    print "ROOT SSH Credentials:"
+    print credentials
+    print "Opening ssh session. Close it to free up allocation"
+    os.system(
+        "sshpass -p %(password)s ssh -o ServerAliveInterval=5 -o ServerAliveCountMax=1 "
+        "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p %(port)d "
+        "%(username)s@%(hostname)s" % credentials)
+finally:
+    allocation.free()
