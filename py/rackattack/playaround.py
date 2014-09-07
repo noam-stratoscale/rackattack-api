9f5fc5def85554ecbebec4c4208ccc78460cc160
Shimshon Zimmerman <shimshon@stratoscale.com>
2014-09-07 12:00:28 +0300

    Playaround tool now supports avoiding an ssh connection

--- py/rackattack/playaround.py
+++ py/rackattack/playaround.py
@@ -5,11 +5,13 @@ from rackattack import api
 connection.discardParamikoLogs()
 connection.discardSSHDebugMessages()
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--label", required=True)
 parser.add_argument("--user", required=True, help="User to report to provider")
 parser.add_argument("--nice", type=float, default=1)
+parser.add_argument("--noSSH", action='store_true', default=False)
 args = parser.parse_args()
 
 print "Connecting"
@@ -24,16 +26,20 @@ print "Done allocating, Waiting for boot to finish"
 try:
     node = allocation.nodes()['node']
     credentials = node.rootSSHCredentials()
-    ssh = connection.Connection(**credentials)
-    ssh.waitForTCPServer()
-    ssh.connect()
-    ssh.close()
     print "ROOT SSH Credentials:"
     print credentials
-    print "Opening ssh session. Close it to free up allocation"
-    os.system(
-        "sshpass -p %(password)s ssh -o ServerAliveInterval=5 -o ServerAliveCountMax=1 "
-        "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p %(port)d "
-        "%(username)s@%(hostname)s" % credentials)
+    if not args.noSSH:
+        ssh = connection.Connection(**credentials)
+        ssh.waitForTCPServer()
+        ssh.connect()
+        ssh.close()
+        print "Opening ssh session. Close it to free up allocation"
+        os.system(
+            "sshpass -p %(password)s ssh -o ServerAliveInterval=5 -o ServerAliveCountMax=1 "
+            "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p %(port)d "
+            "%(username)s@%(hostname)s" % credentials)
+    else:
+        print "Not connecting to machine via SSH. Hit Ctrl-C to close the machine."
+        time.sleep(1000000000)
 finally:
     allocation.free()
