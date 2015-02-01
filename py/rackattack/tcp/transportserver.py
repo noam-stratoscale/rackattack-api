1fc542fc2b90c2f206e9a00d6b9540290beaff14
Shlomi Matichin <shlomi@stratoscale.com>
2015-02-01 15:02:30 +0000

    Convention fix

--- py/rackattack/tcp/transportserver.py
+++ py/rackattack/tcp/transportserver.py
@@ -9,7 +9,8 @@ class TransportProtocol(basic.Int32StringReceiver):
     _HEADER = "!I"
 
     def __init__(self, handler):
-        assert self.MAX_LENGTH == 99999, "MAX_LENGTH parameter was changed - make sure you review the new value"
+        assert self.MAX_LENGTH == 99999, \
+            "MAX_LENGTH parameter was changed - make sure you review the new value"
         self._handler = handler
 
     def connectionMade(self):
