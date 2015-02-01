bdde83aa64775addd2c3b00367147dc841b2c112
Shlomi Matichin <shlomi@stratoscale.com>
2015-02-01 12:27:59 +0200

    Fixed broken test

--- test/test.py
+++ test/test.py
@@ -210,6 +210,7 @@ class Test(unittest.TestCase):
             except:
                 import traceback
                 traceback.print_exc()
+                raise
 
 
 if __name__ == '__main__':
